039cc3929ac9958124f673a04611b5334ee1eb81
Henjo Hoeksma <henjo@upassist.com>
2023-05-02 14:53:40 +0200

    chore: initial commit

---
+++ Classes/Controller/Backend/Module/DocumentationReaderController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace UpAssist\DocumentationReader\Controller\Backend\Module;
+
+use Neos\Flow\Mvc\Controller\ActionController;
+use Neos\Fusion\View\FusionView;
+
+class DocumentationReaderController extends ActionController
+{
+    protected $defaultViewObjectName = FusionView::class;
+
+    public function indexAction()
+    {
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# UpAssist Documentation Reader
+
+A simple backend module for Neos to give editors access to documentation.
